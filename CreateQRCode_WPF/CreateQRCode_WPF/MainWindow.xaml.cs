fa3e7ca7192d775293e05cadc83be6ff17e9daa6
Duc238 <92005265+Duc238@users.noreply.github.com>
2023-04-01 13:57:49 +0700

    Add files via upload

---
+++ CreateQRCode_WPF/CreateQRCode_WPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreateQRCode_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private ImageSource BitmapToImageSource(Bitmap bitmap)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                bitmap.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Bmp);
+                memoryStream.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memoryStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+        }
+        #region Sự kiện click của Button tạo QR code
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator codeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = codeGenerator.CreateQrCode(ImageSource.Source.ToString(), QRCodeGenerator.ECCLevel.Q);
+            QRCode qRCode = new QRCode(qRCodeData);
+            Bitmap QRCodeImage = qRCode.GetGraphic(20);
+            ImgQRCode.Source=BitmapToImageSource(QRCodeImage);
+            //txblQRCodeSource.Text= QRCodeImage.ToString();
+        }
+        #endregion
+    }
+}
